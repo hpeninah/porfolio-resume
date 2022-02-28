7ee482daf2c39a497ea17b57216a9c6cc89ab5ea
Hannah Mulato <72578792+hpeninah@users.noreply.github.com>
2022-02-28 11:11:59 -0500

    Adding typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Image } from 'react-bootstrap';
-import './styles/App.scss';
-import Navigation from './components/navbar';
-import About from './components/about';
-import Skills from './components/skills';
+import { Image } from "react-bootstrap";
+import "./styles/App.scss";
+import Navigation from "./components/navbar";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Typewriter from "typewriter-effect";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Image src={require('./assets/1.png')} alt="logo" />
-      <div className="title">Hello There!</div>
+      <div className="title">
+        <Typewriter
+          options={{
+            strings: ["Hello There!"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        <Image src={require("./assets/1.png")} alt="logo" className="logo" />
+      </div>
       <About />
       <Skills />
     </div>
